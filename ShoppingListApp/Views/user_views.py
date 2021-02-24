04638c6ac2d8a5f72215d3657a64911aeb0679cf
TaherAmlaki <taher.amlaki@gmail.com>
2021-02-24 23:29:08 +0100

    /logout and /account added, small checks added for /register and /login

--- ShoppingListApp/Views/user_views.py
+++ ShoppingListApp/Views/user_views.py
@@ -1,7 +1,9 @@
 from flask import (Blueprint, render_template, flash, 
-                   redirect, url_for)
+                   redirect, url_for, request)
+from flask_login import login_user, logout_user, current_user, login_required
 
 from ShoppingListApp.Models.User import User
+
 from ShoppingListApp.Forms.registerForm import RegistrationForm
 from ShoppingListApp.Forms.loginForm import LoginForm
 
@@ -11,11 +13,16 @@ user_views = Blueprint("user_views", __name__, url_prefix="/user")
 
 @user_views.route("/register", methods=["POST", "GET"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("site.home"))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         username = form.username.data 
         password = form.password.data
-        user = User(username=username, password=User.hash_password(password))
+        user = User()
+        user.username = username
+        user.password = User.hash_password(password)
         user.save()
         flash(f"Account created for '{form.username.data}'.", category="success")
         return redirect(url_for("site.home"))
@@ -24,11 +31,13 @@ def register():
 
 @user_views.route("/login", methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("site.home"))
+
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data 
-        password = form.password.data 
-
+        password = form.password.data
         user = User.find_by_username(username)
         if user is None:
             flash(f"User with username '{username}' is not registered.", 'danger')
@@ -37,5 +46,19 @@ def login():
                 flash("Username and password do not match!", 'danger')
             else:
                 flash("Login successful!", category='success')
-                return redirect(url_for("site.home"))
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next', url_for("site.home"))
+                return redirect(next_page)
     return render_template("login.html", title="Login", form=form)
+
+
+@user_views.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("site.home"))
+
+
+@user_views.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
