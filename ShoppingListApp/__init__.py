7f0a75f6d8faeb8238036c60aadaa13fd8a4c871
TaherAmlaki <taher.amlaki@gmail.com>
2021-02-25 01:15:40 +0100

    Create app imports changes for new structure, create app frm config object

--- ShoppingListApp/__init__.py
+++ ShoppingListApp/__init__.py
@@ -2,23 +2,27 @@ from flask import Flask
 
 from ShoppingListApp.DB.mongodb import db
 
-from ShoppingListApp.Views.user_views import user_views
-from ShoppingListApp.Views.site import site
+from ShoppingListApp.users.login import login_manager
 
-from ShoppingListApp import configs
+from ShoppingListApp.users.views import user_views
+from ShoppingListApp.site.views import site_views
+from ShoppingListApp.shoppingLists.views import shopping_list_views
+
+from ShoppingListApp.configs import Config
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = configs.SECRET_KEY
-    app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["MONGODB_SETTINGS"] = configs.MONGODB_SETTINGS
+    app.config.from_object(Config)
 
     db.init_app(app)
 
+    login_manager.init_app(app)
+
     app.register_blueprint(user_views)
-    app.register_blueprint(site)
+    app.register_blueprint(site_views)
+    app.register_blueprint(shopping_list_views)
 
     return app
 
--- ShoppingListApp/configs.py
+++ ShoppingListApp/configs.py
@@ -1,13 +1,14 @@
 import os
 
 
-MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
-MONGODB_NAME = os.getenv('MONGODB_NAME')
-MONGODB_PORT = os.getenv('MONGODB_PORT')
-MONGODB_SETTINGS = {
-    "host": f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@mongodb:{MONGODB_PORT}/{MONGODB_NAME}?authSource=admin"
-}
+class Config:
+    MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+    MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+    MONGODB_NAME = os.getenv('MONGODB_NAME')
+    MONGODB_PORT = os.getenv('MONGODB_PORT')
+    MONGODB_SETTINGS = {
+        "host": f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@mongodb:{MONGODB_PORT}/{MONGODB_NAME}?authSource=admin"
+    }
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-PROPAGATE_EXCEPTIONS = os.getenv("PROPAGATE_EXCEPTIONS")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    PROPAGATE_EXCEPTIONS = os.getenv("PROPAGATE_EXCEPTIONS")
