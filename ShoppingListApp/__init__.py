3be7c125abcc9b59e8adf005f470aa2153c80336
TaherAmlaki <taher.amlaki@gmail.com>
2021-02-26 03:51:21 +0100

    Change in create app, and blueprints imports

--- ShoppingListApp/__init__.py
+++ ShoppingListApp/__init__.py
@@ -1,25 +1,21 @@
 from flask import Flask
 
 from ShoppingListApp.DB.mongodb import db
-
 from ShoppingListApp.users.login import login_manager
-
-from ShoppingListApp.users.views import user_views
-from ShoppingListApp.site.views import site_views
-from ShoppingListApp.shoppingLists.views import shopping_list_views
-
 from ShoppingListApp.configs import Config
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
 
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
-
     login_manager.init_app(app)
 
+    from ShoppingListApp.users.views import user_views
+    from ShoppingListApp.site.views import site_views
+    from ShoppingListApp.shoppingLists.views import shopping_list_views
     app.register_blueprint(user_views)
     app.register_blueprint(site_views)
     app.register_blueprint(shopping_list_views)
